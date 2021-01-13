9587015104a2b233f0f416df59ac52689c097d12
ybkong98 <75230763+ybkong98@users.noreply.github.com>
2021-01-13 10:36:30 +0900

    Update 2.cpp

--- complexdata/2.cpp
+++ complexdata/2.cpp
@@ -3,6 +3,9 @@
 #include <vector>
 using namespace std;
 int main(){
-  
+  array<char,30> actors;
+  array<short,100> betsie;
+  array<float,13> chuck;
+  array<long double,64> dipsea;
 return 0;
 }
